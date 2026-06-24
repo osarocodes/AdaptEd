0a0d313c5a7a300b7245ff378b32160b1d506a77
Eboizegbe Osaro <eboizegbeosaro1@gmail.com>
2026-06-24 14:20:58 +0100

    feat(models): Create Quiz, Performance, LearningPath, Subject, and User schema

---
+++ AdaptEd/BACKEND/src/models/Performance.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const topicResultSchema = new mongoose.Schema({
+    topic: { type: String, required: true },
+    correct: { type: Number, default: 0 },
+    total: { type: Number, default: 0 },
+});
+
+const performanceSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    subject: { type: String, required: true },
+    score: { type: Number, required: true },
+    topicResults: {topicResultSchema},
+    weakAreas: [{ type: String }],
+    strongAreas: [{ type: String }]
+}, { timestamps: true })
+
+module.exports = mongoose.model('Performance', performanceSchema)
---
+++ AdaptEd/BACKEND/src/models/Quiz.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const questionSchema = new mongoose.Schema({
+    question: { type: String, required: true },
+    options: [{ type: String, required: true }],
+    correctAnswer: { type: String, required: true },
+    topic: { type: String, required: true },
+    difficulty: { type: String, required: true, enum: ['easy', 'medium', 'hard'], default: 'medium'},
+}),
+
+const quizSchema = new mongoose.Schema({
+    subject: { type: String, required: true },
+    classLevel: { type: String, required: true },
+    questions: [questionSchema],
+}, { timestamps: true })
+
+module.exports = mongoose.model('Quiz', quizSchema);
