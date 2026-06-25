0d5909c548f20b62ff137fb3631d280f4d067f38
Eboizegbe Osaro <eboizegbeosaro1@gmail.com>
2026-06-25 15:09:58 +0100

    add Auth endpoint and Onboarding

--- AdaptEd/BACKEND/src/routes/auth.route.js
+++ AdaptEd/BACKEND/src/routes/auth.route.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { registerUser, loginUser, logoutUser, home } from "../controllers/auth.controllers.js";
+import { registerUser, loginUser, logoutUser, onboardUser } from "../controllers/auth.controllers.js";
+import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.put("/onboard", protect, onboardUser)
+
 router.get("/logout", logoutUser);
-
-router.get("/", home)
-
+// router.get("/", home);
 export default router;
