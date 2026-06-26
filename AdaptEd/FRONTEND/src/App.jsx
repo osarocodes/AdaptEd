4c8b700e5df3d38276270b89d7d1c4bf7f6828ab
FOA-developer <flourishagbanoma@gmail.com>
2026-06-26 04:59:55 +0100

    feat: implement routing for login and registration pages in App component

--- AdaptEd/FRONTEND/src/App.jsx
+++ AdaptEd/FRONTEND/src/App.jsx
@@ -1,11 +1,17 @@
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
